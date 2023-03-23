5c27208932eaedfe6388e3186e03ad4c4f226c88
Alexander Lindgren <alexander.lindgren@willandskill.se>
2023-03-23 14:35:04 +0100

    update files

---
+++ other/hemuppgift_generate_data_lect_5.py
@@ -0,0 +1,112 @@
+import csv
+import random
+from datetime import datetime, timedelta
+
+# Helper functions
+
+
+def random_price():
+    return round(random.uniform(10, 500), 2)
+
+def random_date(start, end):
+    return start + timedelta(seconds=random.randint(0, int((end - start).total_seconds())))
+
+def random_index(last_index):
+    return random.randint(0, last_index)
+
+# Define product and customer data
+products = ['T-shirt', 'Jeans', 'Sneakers', 'Jacket', 'Watch']
+
+
+customer_ids = [x for x in range(0,23)]
+
+customer_emails = [
+    'john@example.com',
+    'jane@example.com',
+    'bob@example.com',
+    'johndoe1234@example.com',
+    'sarahjones5678@gmail.com',
+    'brianwilson4321@hotmail.com',
+    'katewilliams7890@outlook.com',
+    'davidlee2468@yahoo.com',
+    'laurensmith1357@gmail.com',
+    'adamjohnson2468@yahoo.com',
+    'amybrown7890@outlook.com',
+    'jenniferwhite1234@gmail.com',
+    'matthewjackson5678@hotmail.com',
+    'chrisgreen2468@outlook.com',
+    'rachelgray7890@yahoo.com',
+    'ericmorris1234@example.com',
+    'stephaniebrown5678@gmail.com',
+    'nickjones2468@outlook.com',
+    'laurawilson7890@example.com',
+    'patricktaylor1357@yahoo.com',
+    'jessicasmith5678@gmail.com',
+    'andrewbrown2468@outlook.com',
+    'emilydavis7890@example.com',
+]
+
+customer_data = []
+
+for x, y in zip(customer_ids, customer_emails):
+    customer = {
+        'customer_id': x,
+        'customer_email': y
+    }
+    customer_data.append(customer)
+
+
+
+
+# Define order count and date range
+order_count = 1000
+start_date = datetime(2022, 1, 1)
+end_date = datetime(2023, 3, 17)
+
+# Generate sales dataset
+orders = []
+for i in range(order_count):
+    order_date = random_date(start_date, end_date)
+    order_id = i + 1
+    customer_id = random.choice(customer_ids)
+    product_count = random.randint(1, 5)
+    product_list = random.sample(products, product_count)
+    total_price = round(sum(random_price() for _ in range(product_count)),2)
+    order = {
+        'order_id': order_id,
+        'customer_id': customer_id,
+        'product_list': ', '.join(product_list),
+        'total_price': total_price,
+        'order_date': order_date.strftime('%m/%d/%Y %H:%M:%S')
+    }
+    orders.append(order)
+
+# Add incomplete data, outliers, duplicate data, and inconsistent date formats
+for x in range(14):
+    orders[random_index(len(orders)- 1)]['product_list'] = ''
+    orders[random_index(len(orders)- 1)]['total_price'] = 10000
+    orders[random_index(len(orders)- 1)]['customer_id'] = ''
+    orders[random_index(len(orders)- 1)]['order_date'] = '01/2022/01 01:00:00'
+
+for x in range(6):
+    rand_index = random_index(len(orders)- 1)
+    order_id_first_order = orders[rand_index]['order_id']
+    other_rand_index = random_index(len(orders)- 1)
+    if not rand_index == other_rand_index:
+        orders[other_rand_index]['order_id'] = order_id_first_order
+
+# Save sales dataset to CSV file
+with open('order_data.csv', mode='w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=[
+                            'order_id', 'customer_id', 'product_list', 'total_price', 'order_date'])
+    writer.writeheader()
+    for order in orders:
+        writer.writerow(order)
+
+
+with open('customer_details.csv', mode='w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=[
+                            'customer_id', 'customer_email',])
+    writer.writeheader()
+    for order in customer_data:
+        writer.writerow(order)
--- other/lektion_5_dataset.py
+++ other/lektion_5_dataset.py
@@ -1,28 +1,67 @@
 import csv
+import random
+import datetime
 
-# Create a CSV file for customer orders
+products = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+]
+
+first_order_id = 1001
+
+def random_date(start, end):
+    return start + datetime.timedelta(seconds=random.randint(0, int((end - start).total_seconds())))
+
+customer_ids = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10
+]
+
 orders_header = ['OrderID', 'CustomerID', 'Product', 'Quantity', 'Price']
-orders_rows = [    [1001, 101, 'Product A', 2, 10.99],
-    [1002, 102, 'Product B', 1, 20.99],
-    [1003, 103, 'Product C', 3, 5.99],
-    [1004, 101, 'Product D', 2, 15.99],
-    [1005, 104, 'Product E', 1, 25.99]
-]
 
-with open('customer_orders.csv', mode='w', newline='') as orders_file:
-    orders_writer = csv.writer(orders_file)
-    orders_writer.writerow(orders_header)
-    orders_writer.writerows(orders_rows)
 
-# Create a CSV file for customer details
-details_header = ['CustomerID', 'Name', 'Address', 'Phone']
-details_rows = [    [101, 'John Doe', '123 Main St', '555-1234'],
-    [102, 'Jane Smith', '456 Elm St', '555-5678'],
-    [103, 'Bob Johnson', '789 Oak St', '555-9012'],
-    [104, 'Alice Brown', '321 Pine St', '555-3456']
-]
+start_date = datetime.datetime(2022, 1, 1)
+end_date = datetime.datetime(2023, 3, 17)
 
-with open('customer_details.csv', mode='w', newline='') as details_file:
-    details_writer = csv.writer(details_file)
-    details_writer.writerow(details_header)
-    details_writer.writerows(details_rows)
+order_rows = []
+for x in range(10):
+    order = [
+        first_order_id + x,
+        random.choice(customer_ids),
+        random.choice(products),
+        random_date(start_date, end_date)
+    ]
+    order_rows.append(order)
+
+print(order_rows)
+
+
+
+# with open('customer_orders.csv', mode='w', newline='') as orders_file:
+#     orders_writer = csv.writer(orders_file)
+#     orders_writer.writerow(orders_header)
+#     orders_writer.writerows(orders_rows)
+
+# # Create a CSV file for customer details
+# details_header = ['CustomerID', 'Name', 'Address', 'Phone']
+# details_rows = [    [101, 'John Doe', '123 Main St', '555-1234'],
+#     [102, 'Jane Smith', '456 Elm St', '555-5678'],
+#     [103, 'Bob Johnson', '789 Oak St', '555-9012'],
+#     [104, 'Alice Brown', '321 Pine St', '555-3456']
+# ]
+
+# with open('customer_details.csv', mode='w', newline='') as details_file:
+#     details_writer = csv.writer(details_file)
+#     details_writer.writerow(details_header)
+#     details_writer.writerows(details_rows)
