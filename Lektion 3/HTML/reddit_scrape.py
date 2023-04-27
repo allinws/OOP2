7c46bd252b22d6132f0db7668aa0d1b69371429f
Alexander Lindgren <alexander.lindgren@willandskill.se>
2023-04-27 14:18:56 +0200

    updated using split instead of regex

--- Lektion 3/HTML/reddit_scrape.py
+++ Lektion 3/HTML/reddit_scrape.py
@@ -1,20 +1,19 @@
 import requests
-import re
 from bs4 import BeautifulSoup
+import pprint
 
 url = "https://www.reddit.com/r/sweden/"
 response = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
-
 soup = BeautifulSoup(response.content, "html.parser")
 
 posts = []
 for post in soup.find_all("div", class_="Post"):
     title = post.find("h3", class_="_eYtD2XCVieq6emjKBH3m").text
     comments = post.find("span", class_="FHCV02u6Cp2zYL0fhQPsO").text
-    number = int(re.search(r'\d+', comments).group())
+    number = int(comments.split()[0])
     obj = {'title': title, 'comments_count': number}
     posts.append(obj)
 
 posts = sorted(posts, key=lambda x: x['comments_count'], reverse=True)
 
-print(posts)
+pprint.pprint(posts, indent=4)
