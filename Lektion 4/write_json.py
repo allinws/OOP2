6c5624aff678414d6b0dc7d91e2b31e7c1994dfa
Alexander Lindgren <alexander.lindgren@willandskill.se>
2023-04-28 13:47:07 +0200

    updated with adding data to json file

--- Lektion 4/write_json.py
+++ Lektion 4/write_json.py
@@ -1,4 +1,12 @@
 import json
+import csv
+import json
+from pathlib import Path
+import sys
+
+current_path = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+
+""" CREATE INTIAL FILE """
 
 data = {
     "name": "Alice",
@@ -7,5 +15,24 @@ data = {
     "courses": ["Math", "Physics", "Programming"]
 }
 
-with open("data.json", "w") as outfile:
+with open(f"{current_path}/data.json", "w") as outfile:
     json.dump(data, outfile, indent=2)
+
+
+""" ADD NEW DATA TO FILE """
+
+
+new_data = {
+    "name": "Bob",
+    "age": 30,
+    "is_student": False,
+    "courses": ["Chemistry", "Biology", "Geology"]
+}
+
+with open(f"{current_path}/data.json", "r") as infile:
+    existing_data = json.load(infile)
+
+existing_data_list = [existing_data, new_data]
+
+with open(f"{current_path}/data.json", "w") as outfile:
+    json.dump(existing_data_list, outfile, indent=2)
